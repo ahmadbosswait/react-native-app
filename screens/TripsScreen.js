016183e66d601868f4679955b7df741c32b47cc8
ahmadbosswait <ahmadbosswait@gmail.com>
2019-03-30 00:11:03 +0000

    adding new menu to app

--- screens/TripsScreen.js
+++ screens/TripsScreen.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { TextInput, View, StyleSheet, Button,Alert } from 'react-native'
+import axios from 'axios';
 
 export class TripsScreen extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		   name: "",
+		};
+	 }
+  
+	 // Occurs when signout is pressed...
+	 sendData = () => {
+		axios({
+			method: 'post',
+			url: 'http://5x5.ir/api/RestUser/users',
+			data: {
+				firstname: this.state.name,
+				img_src: '1895804_09bf_21.jpg',
+				img_src_thumb: '1895804_09bf_21_thumb.jpg'
+			}
+		  })
+		  .then(function (response) {
+			console.log(response.data.data.firstname);
+		  })
+		  .catch(function (error) {
+			console.log(error);
+		  });
+			this.textInput.clear();
+
+	 }
+
+
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Trips!</Text>
-			</View>
+
+            <TextInput style={styles.textInput}
+			   placeholder="Name" autoCapitalize="none" 
+			   ref={input => { this.textInput = input }}
+			   onSubmitEditing={ () => this.sendData() }
+               onChangeText={(text) => { this.setState({ name: text }) }}
+            />
+
+            <Button title="Send" onPress={this.sendData} />
+         </View>
 		)
 	}
 }
@@ -14,10 +53,6 @@ export class TripsScreen extends React.Component {
 export default TripsScreen
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+	text: { color: "white", fontWeight: "bold", textAlign: "center", fontSize: 20, },
+	textInput: { borderWidth:1, borderColor:"gray", marginVertical: 20, padding:10, height:40, alignSelf: "stretch", fontSize: 18, },
+  });
